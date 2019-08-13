097b3f200a76a9a7b878d784134c74e457ee364e
papercut <papercut@172.17.61.69>
2019-08-13 16:30:29 +0700

    2commit

--- src/Chep3/CallEmployee.java
+++ src/Chep3/CallEmployee.java
@@ -1,9 +1,13 @@
 package Chep3;
 
-/**
- *
- * @author papercut
- */
 public class CallEmployee {
-    
+         public static void main(String[] args){
+             System.out.println("Id :"+Employee.id);
+             Employee.work();
+             
+         Employee em = new Employee();
+             System.out.println("Name :"+em.name);
+             System.out.println("Age "+em.age);
+             em.getSalary();
+        }
 }
--- src/Chep3/Employee.java
+++ src/Chep3/Employee.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Chep3;
-
-/**
- *
- * @author papercut
- */
 public class Employee {
+    static int id =1001;
+    String name = "Rusdee";
+    int age = 20;
     
+    static void work(){
+        System.out.println("work()");
+    }
+    
+    void getSalary(){
+         System.out.println("gatSalary()");
+     }
+    
+    public static void main(String[] args){
+        //call static attr & mothod
+        //call non-static
+        System.out.println("Id : "+id);
+        work();
+        
+        Employee em = new Employee();
+        System.out.println("Name : "+em.name);
+        System.out.println("Age : "+em.age);
+        em.getSalary(); 
+    }
 }
